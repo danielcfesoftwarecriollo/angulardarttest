6d18e2df21a1e02e276afe8bc115b07bf03d70df
Daniel Castillo <danielcfe@softwarecriollo.com>
2014-09-22 23:40:54 -0430

    Fix problem with html-component and Polymer ( change version of html-component )

--- lib/testapp.dart
+++ lib/testapp.dart
@@ -3,15 +3,16 @@ library testapp;
 
 import 'dart:async';
 import 'dart:html';
+
+
+
+//import 'package:angular_ui/utils/injectable_service.dart';
 import 'package:polymer/polymer.dart';
-
-
-import 'package:angular_ui/utils/injectable_service.dart';
-import 'package:angular/angular.dart';
 import 'package:angular/routing/module.dart';
 import 'package:angular/application_factory.dart';
 import 'package:angular_ui/angular_ui.dart';
-import 'package:angular_node_bind/angular_node_bind.dart';
+import 'package:angular_node_bind/angular_node_bind.dart' show NodeBindModule;
+import 'package:angular/angular.dart';
 
 
 part 'controllers/testapp_controller.dart';
